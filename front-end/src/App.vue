ab1c9ac6bed138d8809c515861a406449f647a4b
Vinicius Louzada <viniciuslouzada99@hotmail.com>
2025-07-12 16:19:29 -0300

    - Added a toggle between overview and technical analysis.

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,28 +1,40 @@
 <script setup>
 import TechnicalAnalysis from "./components/TA.vue";
+import Overview from "./components/OV.vue";
 import { SymbolStore } from "./stores/symbolStore";
-import { ref } from 'vue'
+import { ref } from "vue";
 
-const selectedSymbol = ref('BTC')
+const selectedSymbol = ref("BTC");
+const selectedView = ref("technical-analysis");
 
 function onSymbolChange() {
-  SymbolStore.value = selectedSymbol.value
+  SymbolStore.value = selectedSymbol.value;
 }
 </script>
 
 <template>
   <div class="sidebar">
-    <select @change="onSymbolChange" v-model="selectedSymbol">
+    <select
+      class="symbol-select"
+      @change="onSymbolChange"
+      v-model="selectedSymbol"
+    >
       <option value="BTC">Bitcoin</option>
       <option value="ETH">Ethereum</option>
     </select>
-    <button class="technical analysis">Technical Analysis</button>
+    <div class="views">
+      <button class="overview" @click="selectedView = 'ov'">Overview</button>
+      <button class="technical-analysis" @click="selectedView = 'ta'">Technical Analysis</button>
+    </div>
   </div>
-  <div class="content"><TechnicalAnalysis></TechnicalAnalysis></div>
+  <div class="content"><TechnicalAnalysis v-if="selectedView === 'ta'" />
+    <Overview v-if="selectedView === 'ov'" /></div>
 </template>
 
 <style scoped>
 .sidebar {
+  display: flex;
+  flex-direction: column;
   width: 15vw;
   height: 100vh;
   background-color: #f0f0f0;
@@ -37,5 +49,32 @@ function onSymbolChange() {
   position: fixed;
   top: 0;
   left: 15vw;
+}
+
+.symbol-select {
+  height: 7vh;
+  width: 100%;
+  border-radius: 0;
+}
+
+.views {
+  margin: 1vh 0 0 0;
+}
+
+.button {
+  width: 1%;
+}
+
+.overview {
+  height: 7vh;
+  width: 100%;
+  border-radius: 0;
+}
+
+.technical-analysis {
+  height: 7vh;
+  width: 100%;
+  border-radius: 0;
+  margin: 1vh 0 0 0;
 }
 </style>
---
+++ front-end/src/components/OV.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+    <div class="main">
+        <h1>Overview</h1>
+    </div>
+</template>
+
+<style scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #000;
+  color: #00ff00;
+  height: 100vh;
+  width: 85vw;
+  font-family: "Courier New", Courier, monospace;
+}
+</style>
